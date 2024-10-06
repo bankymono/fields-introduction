b1367d01bf1f7f0daefc98643ec5d443671396fb
Ayodeji Bankole <Ayodeji.Bankole@Interswitchng.com>
2024-10-06 20:46:01 +0100

    created movie related objects

--- src/main/java/org/bankymono/Main.java
+++ src/main/java/org/bankymono/Main.java
@@ -1,6 +1,35 @@
 package org.bankymono;
 
 public class Main {
+    public static class Movie extends Product {
+        public static final double MINIMUM_PRICE = 10.99;
+        private boolean isReleased;
+        private Category category;
+        private double actualPrice;
+
+        public Movie(String name, int year, double price, boolean isReleased, Category category){
+            super(name,year);
+            this.isReleased = isReleased;
+            this.category = category;
+            this.actualPrice = Math.max(price, MINIMUM_PRICE;
+        }
+
+    }
+
+    public static class Product {
+        protected String name;
+        protected int year;
+        protected double actualPrice;
+
+        public Product(String name, int year){
+            this.name = name;
+            this.year = year;
+        }
+    }
+
+    public enum Category {
+        ADVENTURE,ACTION,COMEDY
+    }
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
